4befce1fccafa4002132891bca87bc99eca137cf
Ashfaque <ashfaque.d@gmail.com>
2023-04-01 23:39:09 +0530

    vpn tested by swagger

--- src/main/java/com/driver/model/Connection.java
+++ src/main/java/com/driver/model/Connection.java
@@ -1,29 +1,22 @@
 package com.driver.model;
 
-import com.driver.model.CountryName;
-import com.driver.model.User;
-
 import javax.persistence.*;
 
-// Note: Do not write @Enumerated annotation above CountryName in this model.
 @Entity
-public class Country{
+public class Connection {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    private CountryName countryName;
-
-    private String code;
-
-    @OneToOne
-  private User user;
+    @ManyToOne
+    @JoinColumn
+    private User user;
 
     @ManyToOne
     @JoinColumn
     private ServiceProvider serviceProvider;
 
-    public Country() {
+    public Connection(){
 
     }
 
@@ -33,22 +26,6 @@ public class Country{
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public CountryName getCountryName() {
-        return countryName;
-    }
-
-    public void setCountryName(CountryName countryName) {
-        this.countryName = countryName;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
     }
 
     public User getUser() {
